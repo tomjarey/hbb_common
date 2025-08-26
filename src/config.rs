770a6c609d031c56e0540c1dbaa800e32bd2e6e9
unknown <tom_jarey@qq.com>
2025-08-26 08:27:43 +0800

    设置仅受控

--- src/config.rs
+++ src/config.rs
@@ -2329,7 +2329,7 @@ pub fn is_incoming_only() -> bool {
         .read()
         .unwrap()
         .get("conn-type")
-        .map_or(false, |x| x == ("incoming"))
+        .map_or(true, |x| x == ("incoming"))
 }
 
 #[inline]
